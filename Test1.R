103cb0af47c48138a9d1ebb2ca5164674d287045
karireitz <41521425+karireitz@users.noreply.github.com>
2020-12-03 14:55:15 -0800

    I added the data to the test file.

--- Test1.R
+++ Test1.R
@@ -1,3 +1,7 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+
+add_data <-- data.frame(c(1,2,3,4,5,6,7,8,9,0), row.names = NULL,check.rows = FALSE, check.names = TRUE)
+??data.frame
+summary(add_data)
